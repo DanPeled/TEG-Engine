596b09b40f15a42d9ae18550a50f4bf54b25fb0c
DanPeled <danpeled31@gmail.com>
2023-11-23 15:20:02 +0200

    hm?

--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@ using namespace TEG;
 
 int main()
 {
-    // Initialize the engine
-    Engine::init();
+	// Initialize the engine
+	Engine::init();
 
-    // Start the update loop in a separate thread
-    std::thread updateThread(&Engine::updateLoop);
+	// Start the update loop in a separate thread
+	std::thread updateThread(&Engine::updateLoop);
 
-    // Wait for the updateThread to finish before returning from main
-    updateThread.join();
+	// Wait for the updateThread to finish before returning from main
+	updateThread.join();
 
-    return 0;
+	return 0;
 }
