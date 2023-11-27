49bb26c1a0ff46a2aea6e022a399b51d359506f4
= <danpeled31@gmail.com>
2023-11-27 17:38:45 +0200

    started working on README, fixed Button class (prob)

--- Engine/Classes/UI/Button.cpp
+++ Engine/Classes/UI/Button.cpp
@@ -1,35 +1,38 @@
-// #include "Button.h"
-// using namespace TEG;
-// template <typename... K>
-// UI::Button::Button(Vector2 position_, std::string text_, WORD color_, void (*func)(K...))
-// 	: Object(position_), text(text_), color(color_), onClick(func)
-// {
-// }
+#include "Button.h"
+using namespace TEG;
+template <typename... K>
+UI::Button::Button(Vector2 position_, std::string text_, WORD color_, void (*func)(K...))
+	: Object(position_), text(text_), color(color_), OnClick(func)
+{
+}
 
-// template <typename... K>
-// UI::Button::Button(Vector2 position_, std::string text_, void (*func)(K...))
-// 	: Object(position_), text(text_), color(ConsoleAttributes().WHITE), onClick(func)
-// {
-// }
+template <typename... K>
+UI::Button::Button(Vector2 position_, std::string text_, void (*func)(K...))
+	: Object(position_), text(text_), color(ConsoleAttributes().WHITE), OnClick(func)
+{
+}
 
-// template <typename... K>
-// UI::Button *UI::Button::Instantiate(Vector2 pos, std::string text, WORD color, void (*func)(K...))
-// {
-// 	UI::Button *obj = new UI::Button(pos, text, color, func);
-// 	Object::objects.push_back(*obj);
-// 	return obj;
-// }
+template <typename... K>
+UI::Button *UI::Button::Instantiate(Vector2 pos, std::string text, WORD color, void (*func)(K...))
+{
+	UI::Button *obj = new UI::Button(pos, text, color, func);
+	Object::objects.push_back(*obj);
+	return obj;
+}
 
-// template <typename... K>
-// UI::Button *UI::Button::Instantiate(Vector2 pos, std::string text, void (*func)(K...))
-// {
-// 	UI::Button *obj = new UI::Button(pos, text, func);
-// 	Object::objects.push_back(obj);
-// 	return obj;
-// }
+template <typename... K>
+UI::Button *UI::Button::Instantiate(Vector2 pos, std::string text, void (*func)(K...))
+{
+	UI::Button *obj = new UI::Button(pos, text, func);
+	Object::objects.push_back(*obj);
+	return obj;
+}
 
-// // Explicit instantiation for the combinations of template arguments you expect to use
-// template UI::Button::Button(Vector2 position_, std::string text_, WORD color_, void (*func)());
-// template UI::Button::Button(Vector2 position_, std::string text_, void (*func)());
-// template UI::Button *UI::Button::Instantiate(Vector2 pos, std::string text, WORD color, void (*func)());
-// template UI::Button *UI::Button::Instantiate(Vector2 pos, std::string text, void (*func)());
+void UI::Button::Render(const CONSOLE_SCREEN_BUFFER_INFO &csbi) const
+{
+}
+// Explicit instantiation for the combinations of template arguments you expect to use
+template UI::Button::Button(Vector2 position_, std::string text_, WORD color_, void (*func)());
+template UI::Button::Button(Vector2 position_, std::string text_, void (*func)());
+template UI::Button *UI::Button::Instantiate(Vector2 pos, std::string text, WORD color, void (*func)());
+template UI::Button *UI::Button::Instantiate(Vector2 pos, std::string text, void (*func)());
--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # TEG-Engine
-> TEG Game Engine - A text based game engine made in C++
-
+> <span style="font-weight: bold; color: gray;">TEG Game Engine - A text based game engine made in C++</span>
 # Make into an EXE: 
 
 **TEG engine is currently only *Windows* supported**
@@ -12,5 +11,28 @@ or
 ```bash
 .\compile.bat
 ```
+
+---
+# Engine.cpp
+> <span style="font-weight: bold; color: gray;">The `Engine` class takes care of the screen rendering and initializing the game & engine.</span>
+## Init
+The `Engine::Init(Game game_)` function starts up the Input class, and calls the start function on the game class provided to it on the main file.
+
+---
+## Update Loop
+On the `Engine::UpdateLoop()` function, the `Engine::PrintScreen()` function
+
+### Print Screen
+The `PrintScreen()` function handles rendering the screen and going through the following steps:
+- Clear the screen.
+- Go through the `Object::objects` refrence vector, and call the Render function on each one.
+
+---
+## Stop
+The `Engine::OnStop()` function handles stopping the engine, and goes through the following steps:
+- starts by calling the `Exit()` function on the game variable (that was passed in the `Engine::Init(Game game_)` function).
+- Clears the screen.
+- Prints out "Engine Stopped." for debug purposes.
+- Cleans up the Input class by calling the `Input::Cleanup()` function.
 
 ---
